e5b85b7d1334a76ff7c140e21b66648ec36921e6
nurtt0949 <nurtt0949@wrdsb.ca>
2020-09-15 17:45:22 +0000

    Calculates dicounts

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program calculates the discounts for customers
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,89 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a Scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user to type in amount of money spent
+    System.out.println("Enter the amount you spent");
     
+    //get integer from user 
+    double amount = input.nextDouble();
+    
+
+  
+
+    //calculate the deal
+    if (amount < 40 ){
+      //discount amount
+      double discount = amount * 0.10;
+
+      //saved amount
+      System.out.println("Your discounted amount is " + discount);
+
+      double total = amount-discount;
+
+      //Final amount
+      System.out.println("Your final price is " + total);
+
+
+
+    }
+    //calculate the deal
+      else if(amount < 80 ){
+      //discount amount
+      double discount = amount * 0.20;
+
+      //saved amount
+      System.out.println("Your discounted amount is " + discount);
+
+      double total = amount-discount;
+
+      //Final amount
+      System.out.println("Your final price is " + total); 
+
+    
+    
+
+    
+    }
+
+    //calculate the deal
+      else if(amount < 120 ){
+      //discount amount
+      double discount = amount * 0.30;
+
+      //saved amount
+      System.out.println("Your discounted amount is " + discount);
+
+      double total = amount-discount;
+
+      //Final amount
+      System.out.println("Your final price is " + total); 
+
+    
+    
+
+    
+    }
+
+    //calculate the deal
+     else if(amount > 120){
+      //discount amount
+      double discount = amount * 0.40;
+
+      //saved amount
+      System.out.println("Your discounted amount is " + discount);
+
+      double total = amount-discount;
+
+      //Final amount
+      System.out.println("Your final price is " + total); 
+
+    
+    
+
+    
+    }
   }
 }
